3cac799b991d796f694493fef8b6864c8a862cf7
Maxime Jacquet <mjacquet@mjacquet-ltm1.internal.salesforce.com>
2018-01-03 11:47:01 +0100

    New home page at scratch org opening

--- orgInit.sh
+++ orgInit.sh
@@ -2,4 +2,4 @@ sfdx force:org:create -f config/project-scratch-def.json -s -d 1
 sfdx force:source:push
 
 sfdx msm:user:password:set -p AG2RLM18 -g User -l User --json
-sfdx force:org:open -p /one/one.app#/setup
+sfdx force:org:open -p /one/one.app#/setup/home
