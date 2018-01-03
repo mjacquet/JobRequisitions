fca7df7a92e623bf0c773b1ada8d52c459a0e744
Maxime Jacquet <mjacquet@mjacquet-ltm1.internal.salesforce.com>
2018-01-03 12:00:24 +0100

    with very basic Data Model

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,1 @@
-sfdx force:org:create -f config/project-scratch-def.json -s -d 1
-sfdx force:source:push
-
-sfdx msm:user:password:set -p AG2RLM18 -g User -l User --json
-sfdx force:org:open -p /one/one.app#/setup/home
+sfdx force:source:push -u <SOUSERNAME>
