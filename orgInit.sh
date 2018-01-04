b5422005edaa1a8efe50fd113278940a39e5d0d1
Maxime Jacquet <mjacquet@mjacquet-ltm1.internal.salesforce.com>
2018-01-04 14:54:48 +0100

    fix orginit

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,3 @@
-sfdx force:source:push
-sfdx force:user:permset:assign -n Gestion_des_embauches
 sfdx force:user:create --definitionfile data/userdef.json LastName=Responsable FirstName=Julie Email=jres@ag2rlm.co Username=jres@ag2rlm.org CommunityNickname=Julie Alias=Julie
 sfdx force:user:create --definitionfile data/userdef.json LastName=RH FirstName=Jean Email=jrh@ag2rlm.co Username=jrh@ag2rlm.org CommunityNickname=Jean Alias=Jean
 sfdx force:data:tree:import --plan data/Job_Requisition__c-Applicant__c-plan.json
