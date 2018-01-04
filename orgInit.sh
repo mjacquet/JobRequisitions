7432573b0fa9a55ed6a49e33b297ec4926a04ca2
Maxime Jacquet <mjacquet@mjacquet-ltm1.internal.salesforce.com>
2018-01-04 15:24:33 +0100

    initial cleanup

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,1 @@
-sfdx force:user:create --definitionfile data/userdef.json LastName=Responsable FirstName=Julie Email=jres@ag2rlm.co CommunityNickname=Julie Alias=Julie
-sfdx force:user:create --definitionfile data/userdef.json LastName=RH FirstName=Jean Email=jrh@ag2rlm.co CommunityNickname=Jean Alias=Jean
-sfdx force:data:tree:import --plan data/Job_Requisition__c-Applicant__c-plan.json
-sfdx force:apex:execute -f scripts/setup.cls
+sfdx force:source:push
