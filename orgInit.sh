6e78e9d7f6088a13884b8adc372f218add15f781
Maxime Jacquet <mjacquet@mjacquet-ltm1.internal.salesforce.com>
2018-01-04 14:57:33 +0100

    remive username def from user creation

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:user:create --definitionfile data/userdef.json LastName=Responsable FirstName=Julie Email=jres@ag2rlm.co Username=jres@ag2rlm.org CommunityNickname=Julie Alias=Julie
-sfdx force:user:create --definitionfile data/userdef.json LastName=RH FirstName=Jean Email=jrh@ag2rlm.co Username=jrh@ag2rlm.org CommunityNickname=Jean Alias=Jean
+sfdx force:user:create --definitionfile data/userdef.json LastName=Responsable FirstName=Julie Email=jres@ag2rlm.co CommunityNickname=Julie Alias=Julie
+sfdx force:user:create --definitionfile data/userdef.json LastName=RH FirstName=Jean Email=jrh@ag2rlm.co CommunityNickname=Jean Alias=Jean
 sfdx force:data:tree:import --plan data/Job_Requisition__c-Applicant__c-plan.json
 sfdx force:apex:execute -f scripts/setup.cls
