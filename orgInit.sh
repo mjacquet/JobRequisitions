88f8fbcf1b1dd02c4ec55555255d3479d0d889ff
Maxime Jacquet <mjacquet@mjacquet-ltm1.internal.salesforce.com>
2018-01-19 14:07:29 +0100

    update

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sfdx force:org:create -f config/project-scratch-def.json -s -d 1
 sfdx force:source:push
-sfdx msm:user:password:set -p SFPLATFORM -g User -l User --json
+sfdx msm:user:password:set -p SFPL2018 -g User -l User --json
 sfdx force:org:display --verbose
 sfdx force:org:open
